36409a35c9da798da7823a70ef3e238f1f9bb7e6
ThomasKestler <19tkestler@greenwichcsd.org>
2016-12-08 20:05:25 -0500

    Initial Commit
    did all that jaz

---
+++ src/FunNumbers.java
@@ -0,0 +1,44 @@
+
+public class FunNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//
+//		int iVal = 100;
+//		int iVal2 = iVal;
+
+//		System.out.println(iVal);
+//		System.out.println(iVal2);
+//		iVal = 300;
+//		System.out.println(iVal);
+//		System.out.println(iVal2);
+//		
+//		byte bVal = 100;
+//		long lVal = 123456789;
+//		lVal = bVal;
+//		bVal = (byte)lVal;
+//		
+//		float fVal1 = 13;
+//		float fVal2 = 5;
+//		
+//		System.out.println(fVal1/fVal2);
+//
+//		short sVal1 = 13;
+//		short sVal2 = 5;
+//		
+//		System.out.println(sVal1/sVal2);
+//		
+//		System.out.println(fVal1/sVal2);
+		
+		int iVal = 5;
+		System.out.println(iVal++);
+		System.out.println(iVal);
+		System.out.println(++iVal);
+		System.out.println(iVal+5);
+		
+		System.out.println(iVal += 5);
+		System.out.println(iVal);
+		
+	}
+
+}
